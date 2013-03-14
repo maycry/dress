fd667212b85154c34ab959b934c64e5968958a91
Tanskiy Yuriy <byorickq@gmail.com>
2013-03-15 00:23:42 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dress::Application.config.secret_token = '0e060952b4fe204d337dea87bcab0d742782de1cb880295acf347cef8d6de3e3a13e8d0abd2b2927e9453b57516f52fd0554252a74b94a2a56a0ff0731694686'
