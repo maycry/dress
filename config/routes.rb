f08a977f75d10de5a518efc998b016bb2169abfc
Tanskiy Yuriy <byorickq@gmail.com>
2013-03-25 01:06:26 +0200

    added static controller and quality page

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,8 @@ Dress::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'products#index'
+  root :to => 'static#home'
+  match ':action' => 'static#:action'
 
   # See how all your routes lay out with "rake routes"
 
