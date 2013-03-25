5c196279726aa3ab35d9dd8574a4c7cafecb2916
Tanskiy Yuriy <byorickq@gmail.com>
2013-03-25 21:55:06 +0200

    Revert "Revert "paperclip path fix""
    This reverts commit 5c20222257993365e3abb45feb80d2aeab5c64ca.

--- app/models/attached_image.rb
+++ app/models/attached_image.rb
@@ -1,5 +1,10 @@
 class AttachedImage < ActiveRecord::Base
 	belongs_to :product
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, 
+		:styles => {:thumb5 => "188x251#", 
+					:thumb4 => "238x318#", 
+					:thumb3 => "322x431#"}, 
+		:url  => "/products/:id/:style/:basename.:extension",
+		:path => ":rails_root/public/products/:product_id/:style/:basename.:extension"
 	attr_accessible :image, :image_file_name
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
 	has_many :attached_images, :dependent => :destroy
 	accepts_nested_attributes_for :attached_images, :allow_destroy => true
-	attr_accessible :name, :attached_images_attributes
+	attr_accessible :name, :attached_images_attributes, :price
 end
