ef87e3951e53b1826e06d9d450a7e8e8f4e31e24
Tanskiy Yuriy <byorickq@gmail.com>
2013-03-25 21:54:13 +0200

    Revert "paperclip url fix"
    This reverts commit e1db00059099ba580d42d7f8d1f3de78a3ad6695.

--- app/models/attached_image.rb
+++ app/models/attached_image.rb
@@ -4,7 +4,7 @@ class AttachedImage < ActiveRecord::Base
 		:styles => {:thumb5 => "188x251#", 
 					:thumb4 => "238x318#", 
 					:thumb3 => "322x431#"}, 
-		:url  => "/products/:product_id/:style/:basename.:extension",
+		:url  => "/products/:id/:style/:basename.:extension",
 		:path => ":rails_root/public/products/:product_id/:style/:basename.:extension"
 	attr_accessible :image, :image_file_name
 end
