f0dc738ae2badd3d36e924888ada52032da12eff
Tanskiy Yuriy <byorickq@gmail.com>
2013-03-29 16:03:53 +0200

    htmling product page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,14 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 module ProductsHelper
-	def is_loggedin
-		session[:log_in] ? session[:log_in] : nil
+	
+	# 4000 грн or 10 000 грн
+	def return_price(product)
+		price = number_to_currency product, :unit => "", :precision => 0, :delimiter => " "
+		price.strip!
+		price.length < 6 ? price.gsub!(/\s+/, "") : price
+		price += " грн"
+		content_tag :p, price, :class => "price"
 	end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,5 @@
 module SessionHelper
+	def is_loggedin
+		session[:log_in] ? session[:log_in] : nil
+	end
 end
