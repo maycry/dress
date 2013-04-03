d2a4dce760bee1dcd12d68e6763d974baedce8ec
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-03 16:04:34 +0300

    adding products

--- app/assets/javascripts/admin.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-	$("#product_type_id_input").append("<a href='#'>Create new type</a>")
-	$("#product_type_id_input a").click(function(){
-		$("#product_type_attributes_name_input").toggleClass("hide")
-	})
-
-});
