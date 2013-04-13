cf962ed75a31544d21f0429ae3159501224d8290
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-13 15:39:45 +0300

    added reivews zoomer

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,17 +1,20 @@
 $(function(){
 	var popup
 	var scrollY
-	
-	$('.b-popup').addClass('hidden');
-	$('.sample-item').click(function(){
+	$('.b-popup').each(function(){
+		$(this).addClass('hidden');
+	});
+	$('.sample-item,.review-image').click(function(){
 		popup = $(this).data('slider');
 		$('.'+popup).removeClass('hidden');
 		$('body').addClass('show-popup');
 		scrollY = $('body').scrollTop();
+		return false;
 	});
 	$('.b-popup .close').click(function(){
 		$('body').removeClass('show-popup');
 		$('.'+popup).addClass('hidden');
 		$('body').scrollTop(scrollY);
+		return false;
 	});
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,8 @@ class ProductsController < ApplicationController
   def home
     @products = Product.order("created_at desc").page(1)
     @types = Type.all
+    @review_images = Dir.glob("app/assets/images/reviews/1/*")
+    @review_images.each {|i| i.slice!("app/assets/images/")}
   end
 
   def index
@@ -22,5 +24,7 @@ class ProductsController < ApplicationController
     @images_replica = @product.attached_images.find_all_by_replica(true)
     @products = Product.where("type_id = ? AND style_id = ?", @product.type, @product.style).limit(5)
     @types = Type.all
+    @review_images = Dir.glob("app/assets/images/reviews/1/*")
+    @review_images.each {|i| i.slice!("app/assets/images/")}
   end
 end
