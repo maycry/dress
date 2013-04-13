29b9195195e42b6db7b47e2f02703f0ed16358ef
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-13 15:57:39 +0300

    overlay fixed

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,20 +1,16 @@
 $(function(){
 	var popup
-	var scrollY
 	$('.b-popup').each(function(){
 		$(this).addClass('hidden');
 	});
 	$('.sample-item,.review-image').click(function(){
 		popup = $(this).data('slider');
 		$('.'+popup).removeClass('hidden');
-		$('body').addClass('show-popup');
-		scrollY = $('body').scrollTop();
 		return false;
 	});
 	$('.b-popup .close').click(function(){
 		$('body').removeClass('show-popup');
 		$('.'+popup).addClass('hidden');
-		$('body').scrollTop(scrollY);
 		return false;
 	});
 });
