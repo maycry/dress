1880df38fae5157897a8768c4452392149b7cab5
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-14 23:34:17 +0300

    type chooser disabled

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function($) {
+/*jQuery(document).ready(function($) {
 	$('.b-type-chooser a').click(function(){
 		$('.b-type-chooser a').removeClass('chosen')
 		$(this).addClass('chosen')
 		return false
 	})
-});
+});*/
