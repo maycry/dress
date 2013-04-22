e5ef43d49efc3a73a1113455c6598fef81f745cb
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-22 19:21:49 +0300

    titles

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -6,5 +6,8 @@ class DesignersController < ApplicationController
     @styles = Style.all
     @designers = Designer.all
     @products = Product.order("created_at desc").where("designer_id = ?", @designer).page params[:page]
+    @selections = Selection.all
+    @image1 = Product.selection_image(@selections.first)
+    @image2 = Product.selection_image(@selections.last)
   end
 end
--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -3,8 +3,11 @@ class StylesController < ApplicationController
     @style = Style.find_by_alias!(params[:id])
     @types = Type.all
     @type = Type.find_by_alias!(params[:type_id])
-    @products = Product.order("created_at desc").where("style_id = ?", @style).page params[:page]
     @designers = Designer.all
     @styles = Style.all
+    @products = Product.order("created_at desc").where("style_id = ?", @style).page params[:page]
+    @selections = Selection.all
+    @image1 = Product.selection_image(@selections.first)
+    @image2 = Product.selection_image(@selections.last)
   end
 end
