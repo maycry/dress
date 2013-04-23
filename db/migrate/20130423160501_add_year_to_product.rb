b65ee7979bea7eb710e23b7e0911255063fb712b
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-23 20:37:55 +0300

    year

--- db/migrate/20130423160501_add_year_to_product.rb
+++ db/migrate/20130423160501_add_year_to_product.rb
@@ -1,5 +1,5 @@
 class AddYearToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :year, :string
+    add_column :products, :year, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423163054) do
+ActiveRecord::Schema.define(:version => 20130423160501) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string    "resource_id",   :null => false
@@ -75,17 +75,17 @@ ActiveRecord::Schema.define(:version => 20130423163054) do
   end
 
   create_table "products", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.decimal  "price",        :default => 0.0
-    t.string   "code"
-    t.integer  "type_id"
-    t.integer  "style_id"
-    t.integer  "designer_id"
-    t.text     "description"
-    t.integer  "selection_id"
-    t.string   "year"
+    t.string    "name"
+    t.timestamp "created_at",                    :null => false
+    t.timestamp "updated_at",                    :null => false
+    t.decimal   "price",        :default => 0.0
+    t.string    "code"
+    t.integer   "type_id"
+    t.integer   "style_id"
+    t.integer   "designer_id"
+    t.text      "description"
+    t.integer   "selection_id"
+    t.integer   "year"
   end
 
   create_table "selections", :force => true do |t|
