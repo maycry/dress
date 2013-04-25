43dfdc291c9e1fda09c58292d914964f10b5db48
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-25 22:32:27 +0300

    new passwords

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "urattatta",
-  :password             => "combi1988",
+  :password             => ENV['MAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
