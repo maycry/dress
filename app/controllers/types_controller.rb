4bea33eaf95bd4b44d5e371d8c58a6664336bc25
Tanskiy Yuriy <byorickq@gmail.com>
2013-04-27 12:10:43 +0300

    other in end

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -3,7 +3,7 @@ class TypesController < ApplicationController
     @type = Type.find_by_alias!(params[:id])
     @types = Type.all
     @products = Product.order("created_at desc").where("type_id = ?", @type).page params[:page]
-    @designers = Designer.all
+    @designers = Designer.order("name")
     @styles = Style.all
     @selections = Selection.all
   end
