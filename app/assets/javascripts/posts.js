16d28f7229e70a42527226c0493cc1e4bb8856b9
Tanskiy Yuriy <byorickq@gmail.com>
2013-05-29 22:19:06 +0300

    preview photos

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -5,9 +5,9 @@ jQuery(document).ready(function($) {
 
 	s = $("#post_body").val()
 	if (s != undefined) {
-		a = s.match(/[a-zA-Z0-9]*.(jpg|png)/g);
+		a = s.match(/[a-zA-Z0-9А-Я-Ёа-я-ё]*.(jpg|png)/g);
 		a.map(function(item){
-			$("#thumbs").append("<tr><td><img src='https://s3.amazonaws.com/makemydress.com.ua/blog/"+ item +"'></td></tr>")
+			$("#thumbs").append("<img width='70' src='https://s3.amazonaws.com/makemydress.com.ua/blog/"+ item +"'>")
 		});
 	};
 
